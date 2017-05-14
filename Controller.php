539cdc114fb3428f9d195e39a859e7fb0c3b07e2
ahrchen <ahrchen@gmail.com>
2017-05-13 22:32:11 -0700

    Create Controller.php

---
+++ Controller.php
@@ -0,0 +1,32 @@
+<?php
+include_once("model/Model.php");
+
+class Controller {
+	public $model;
+
+	public function __construct()  
+	{
+		$this->model = new Model();
+	}
+
+	public function invoke()
+	{
+		//functions go in here
+			if(!isset($_GET['visitor']))
+		{
+			//no special visitors were requested, we'll show a list of all visitors
+			$visitors = $this->model->getVisitorList();
+			include 'view/visitorlist.php';
+		}
+		else
+		{
+			//show the requested visitor, probably include a button to sign out.
+			$visitor = $this->model->getVisitor($_GET['visitor']);
+			include 'view/viewVisitor.php';
+
+			//call function to pull in information for new visit, need new view page for login form
+		}
+	}
+}
+
+?>

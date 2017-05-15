f56faed504c558be824c2b6caef3edb243d47404
ahrchen <ahrchen@gmail.com>
2017-05-14 17:28:53 -0700

    Create signOut.php

---
+++ view/signOut.php
@@ -0,0 +1,21 @@
+
+<table style="width: 50%; float: right;">
+	<caption><h3>Sign Out</h3></caption>
+	<thead>
+		<tr>
+			<th>Visitor Name</th>
+			<th>Host Name</th>
+			<th>Sign Out</th>
+
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><input autofocus required visitorName="visitorName"></td>
+			<td><input required hostName="hostName"></td>
+			<td><input type="submit" name="submit" value="submit"></td>
+		</tr>
+
+</tbody>
+</table>
+</div>

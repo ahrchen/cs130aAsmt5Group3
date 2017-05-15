0f561369e4d3d548caca6d2f458cc6587ee53593
ahrchen <ahrchen@gmail.com>
2017-05-14 17:27:58 -0700

    Create viewVisitor.php

---
+++ view/viewVisitor.php
@@ -0,0 +1,15 @@
+<html>
+<head></head>
+
+<body>
+
+<?php 
+
+	echo 'Visitor Name:' . $visitor->visitorName . '<br/>';
+	echo 'Host Name:' . $visitor->hostName . '<br/>';
+	echo 'Sign In Time:' . $visitor->signInTime . '<br/>';
+
+?>
+
+</body>
+</html>

28ffc30d8180a685541d352d2f10f3e5084f0df7
ahrchen <ahrchen@gmail.com>
2017-05-14 17:28:20 -0700

    Create header.php

---
+++ view/header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Group Project 5: Group Number 3</title>
+<style>
+table, th, td {
+    border: 1px solid black;
+     border-collapse: collapse;
+     margin: 0 auto;
+}
+</style>
+
+</head>
+<body>
+<h1 style="text-align: center;">Group Project 5: Group Number 3</h1>

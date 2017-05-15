42f3416bb948c39627cf646197e476baa058abbc
ahrchen <ahrchen@gmail.com>
2017-05-14 17:28:44 -0700

    Create signIn.php

---
+++ view/signIn.php
@@ -0,0 +1,21 @@
+<br><br>
+<div id=signInsignOut style="width:900px;margin:0 auto;clear:both;">
+<table style="width: 50%; float: left;">
+	<caption><h3>Sign In</h3></caption>
+	<thead>
+		<tr>
+			<th>Visitor Name</th>
+			<th>Host Name</th>
+			<th>Sign In</th>
+
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><input autofocus required visitorName="visitorName"></td>
+			<td><input required hostName="hostName"></td>
+			<td><input type="submit" name="submit" value="submit"></td>
+		</tr>
+
+</tbody>
+</table>

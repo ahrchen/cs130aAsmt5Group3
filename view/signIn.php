17a33542438d959aa710cd15f2e21365ece66e5b
ahrchen <ahrchen@gmail.com>
2017-05-16 07:57:37 -0700

    Update signIn.php

--- view/signIn.php
+++ view/signIn.php
@@ -1,5 +1,5 @@
 <br><br>
-<div id=signInsignOut style="width:550px;margin:0 auto;clear:both;">
+<div id=signInsignOut style="width:450px;margin:0 auto;clear:both;">
 <table style="width: 100%; float: left;">
 	<caption><h3>Sign In</h3></caption>
 	<thead>
@@ -12,9 +12,11 @@
 	</thead>
 	<tbody>
 		<tr>
-			<td><input autofocus required visitorName="visitorName"></td>
-			<td><input required hostName="hostName"></td>
-			<td><input type="submit" name="submit" value="submit"></td>
+			<form action="index.php" method="GET" id="signInForm">
+			<td><input autofocus required name="visitorName"></td>
+			<td><input required name="hostName"></td>
+			</form>
+			<td><input type="submit" form="signInForm" name="submit" value="submit"></td>
 		</tr>
 
 </tbody>

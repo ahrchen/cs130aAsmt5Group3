7dbde2c0a27942f1474c5a8059fdb9f93b7f37af
ahrchen <ahrchen@gmail.com>
2017-05-15 22:37:52 -0700

    Update Controller.php

--- controller/Controller.php
+++ controller/Controller.php
@@ -11,10 +11,7 @@ class Controller {
 
 	public function invoke()
 	{
-		//functions go in here
-			if(!isset($_GET['visitor']))
-		{
-			//no special visitors were requested, we'll show a list of all visitors
+		
 			include 'view/header.php';
 			include 'view/signIn.php';
 			include 'view/signOut.php';
@@ -25,15 +22,8 @@ class Controller {
 
 			include 'view/footer.php';
 
-		}
-		else
-		{
-			//show the requested visitor, probably include a button to sign out.
-			$visitor = $this->model->getVisitor($_GET['visitor']);
-			include 'view/viewVisitor.php';
-
-			//call function to pull in information for new visit, need new view page for login form
-		}
+		
+		
 	}
 }
 
